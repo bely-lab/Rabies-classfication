c2f9c6b665aaf7bf6a21720e890dc0e5aafc8b62
bely-lab <tslatebely@gmail.com>
2026-08-27 23:01:12 +0300

    commenting

--- variable_audit.py
+++ variable_audit.py
@@ -18,7 +18,7 @@ for disease, path in files.items():
 
 # ---------------------------------------------------------
 # Create a compact audit of every common variable
-# ---------------------------------------------------------
+# 
 
 columns = datasets["Rabies"].columns
 
