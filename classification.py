e2265599c6e178586e18a59cbad84a535ce48b5c
bely-lab <tslatebely@gmail.com>
2026-09-06 21:44:25 +0300

    comt

--- classification.py
+++ classification.py
@@ -72,7 +72,6 @@ print("1 = Rabies")
 print("0 = Non-Rabies (Anthrax + Brucellosis)")
 
 
-# ============================================================
 # 3. SELECT STRUCTURED FEATURES
 
 # These are the final structured predictors.
